ac14524aa6466559ec98c3f15ea9ed0aad3031ac
Jamie Love <drjlove@gmail.com>
2010-11-03 21:50:44 +1300

    Work around an issue where event handlers may fire prior to the scene graph being completed, causing null pointer exceptions. This workaround/hack ensures that the event handlers are not installed until we've completed building our scene graph.

--- protovis-d3.3.js
+++ protovis-d3.3.js
@@ -1,4 +1,4 @@
-// 40c6f5ffe4b15a3df4723b1de6a55d8730f44c95
+// 6ceba1aea3ec63e0c521d731ace18b2f2e140823
 /**
  * @class The built-in Array class.
  * @name Array
@@ -6102,6 +6102,7 @@ pv.SvgScene.pathJoin = function(s0, s1, s2, s3) {
 };
 pv.SvgScene.panel = function(scenes) {
   var g = scenes.$g, e = g && g.firstChild;
+  var complete = false;
   for (var i = 0; i < scenes.length; i++) {
     var s = scenes[i];
 
@@ -6131,11 +6132,50 @@ pv.SvgScene.panel = function(scenes) {
             var frag = document.createDocumentFragment(true);
 
             g.addEventListener('SVGLoad', function() {
-                this.appendChild(frag);
-                for (var j = 0; j < pv.Scene.events.length; j++) {
-                  this.addEventListener(pv.Scene.events[j], pv.SvgScene.dispatch, false);
+                /**
+                 * This hack was based off a suggestion by Idearat <scott.shattuck@gmail.com>
+                 * on the protovis mailing list to work around the SVGWeb
+                 * SVGLoad event being called prior to the scene graph being
+                 * completed, and causing errors when listeners executed. As
+                 * described by him:
+                 *
+                        So I've been running into a consistent issue with protovis + svgweb
+                        integration where I get an empty (white screen) as many folks have
+                        previously reported.
+
+                        The section of that code with the if (pv.renderer() == "svgweb") block
+                        follows the advice for SVGWeb integration which says to a)
+                        addEventListener('SVGLoad', ...) and to use svgweb.appendChild(). The
+                        problem is that the svgweb.appendChild call immediately turns around
+                        and invokes any event listeners on the newly appended nodes...and the
+                        "frag" can still be empty at that point. This can be confirmed in a
+                        debugger's stack trace.
+
+                        This new version effectively says run the (now embedded in func)
+                        handler logic immediately if complete, otherwise try again shortly
+                        via setTimeout every 100ms until we've completed the scene graph.
+
+                        I've run this through a number of graphs within a complex application
+                        with PHP, JQuery, YUI and other libs in the mix and it consistently
+                        draws the graphs with no more white/blank display.
+
+                * Until a better solution comes along, lets use this.
+                */
+                var me = this;
+                var callback = function () {
+                    if (complete) {
+                        complete = false;
+                        me.appendChild(frag);
+                        for (var j = 0; j < pv.Scene.events.length; j++) {
+                          me.addEventListener(pv.Scene.events[j], pv.SvgScene.dispatch, false);
+                        }
+                        scenes.$g = me;
+                    } else {
+                        setTimeout(callback, 10);
+                    }
                 }
-                scenes.$g = this;
+                callback();
+
             }, false);
 
             svgweb.appendChild (g, s.canvas);
@@ -6207,6 +6247,7 @@ pv.SvgScene.panel = function(scenes) {
       e = c.nextSibling;
     }
   }
+  complete = true;
   return e;
 };
 
